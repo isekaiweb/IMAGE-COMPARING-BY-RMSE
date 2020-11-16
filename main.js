bb14e117f0725e6de47f99d8f3b9743fef876676
isekaiweb <cobasaja271@gmail.com>
2020-11-16 14:32:47 +0700

    perbaikan fungsi

--- main.js
+++ main.js
@@ -3,33 +3,34 @@ class Data {
   constructor(canvas, src) {
     this.canvas = canvas;
     this.src = src;
-
-    this.context = this.canvas.getContext("2d");
     this.img = new Image();
-    this.src.addEventListener("change", (e) => {
-      this.img.src = URL.createObjectURL(e.target.files[0]);
-      this.img.onload = () => {
-        this.context.drawImage(
-          this.img,
-          0,
-          0,
-          this.canvas.width,
-          this.canvas.height
-        );
-        this.getRGB();
-      };
+    this.ctx = this.canvas.getContext("2d");
+
+    this.img.addEventListener("load", () => {
+      this.ctx.drawImage(this.img, 0, 0, this.canvas.width, this.canvas.height);
+      this.getRGB();
     });
+    this.src.addEventListener("change", (e) => this.createImg(e));
+    this.src.addEventListener("click", () => this.removeImg());
+  }
+
+  createImg(e) {
+    this.img.src = URL.createObjectURL(e.target.files[0]);
+  }
+
+  removeImg() {
+    if (this.src.value != "") {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.src.value = "";
+    }
   }
 
   //fungsi untuk mengambil nilai rgba
   getRGB() {
     const rgba = { r: [], g: [], b: [], a: [] },
-      pixelData = this.context.getImageData(
-        0,
-        0,
-        this.canvas.width,
-        this.canvas.height
-      );
+      pixelData = this.canvas
+        .getContext("2d")
+        .getImageData(0, 0, this.canvas.width, this.canvas.height);
     for (let i = 0; i < pixelData.data.length; i += 4) {
       rgba.r.push(pixelData.data[i]);
       rgba.g.push(pixelData.data[i + 1]);
@@ -43,10 +44,10 @@ class Data {
 
 const canvas = document.querySelectorAll("canvas"),
   src = document.querySelectorAll("input[type=file]"),
-  btn = document.querySelector("button");
-
-let data1 = new Data(canvas[0], src[0]),
+  btn = document.querySelector("button"),
+  data1 = new Data(canvas[0], src[0]),
   data2 = new Data(canvas[1], src[1]);
+
 function hasilRMSE() {
   const img1 = data1.getRGB(),
     img2 = data2.getRGB();
@@ -66,23 +67,6 @@ function hasilRMSE() {
   return Math.sqrt((red + blue + green + alpha) / (img1.r.length * 4)); //angka disesuaikan sebelumnya 4
 }
 
-src[0].onclick = () => {
-  if (src[0].value != "") {
-    canvas[0]
-      .getContext("2d")
-      .clearRect(0, 0, canvas[0].width, canvas[0].height);
-    src[0].value = "";
-  }
-};
-src[1].onclick = () => {
-  if (src[1].value != "") {
-    canvas[1]
-      .getContext("2d")
-      .clearRect(0, 0, canvas[1].width, canvas[1].height);
-    src[1].value = "";
-  }
-};
-
 ///ketika tombol btn diklik
 btn.addEventListener("click", () => {
   let hasil = hasilRMSE();
@@ -100,11 +84,14 @@ btn.addEventListener("click", () => {
         btn.innerHTML = "Beda";
       }
     }
+    const h1 = document.querySelector("h1");
+    h1.innerHTML = `RMSE = ${hasil}`;
     setTimeout(() => {
       btn.innerHTML = "Compare";
       src.forEach((i) => {
         i.style.display = "block";
         i.value = "";
+        h1.innerHTML = `COMPARING TWO IMAGES WITH RSME`;
       });
       canvas.forEach((i) => {
         i.getContext("2d").clearRect(0, 0, i.width, i.height);
